3f54671ab9f10ebbf026f9bed41a00b6b0ef0a13
idz <idz@iosdeveloperzone.com>
2014-10-09 02:18:08 -0700

    First attempt at Travis build

--- libvorbis_build.bash
+++ libvorbis_build.bash
@@ -1,21 +1,24 @@
-mkdir -p libvorbis/1.3.3
-pushd libvorbis/1.3.3
-curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz
-tar xvfz libvorbis-1.3.3.tar.gz
+IDZ_VORBIS_VERSION=1.3.3
+IDZ_SDK_VERSION=8.0
+
+mkdir -p libvorbis/$IDZ_VORBIS_VERSION
+pushd libvorbis/$IDZ_VORBIS_VERSION
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-$IDZ_VORBIS_VERSION.tar.gz
+tar xvfz libvorbis-$IDZ_VORBIS_VERSION.tar.gz
 
 # Phone builds
 export IDZ_EXTRA_CONFIGURE_FLAGS=--with-ogg=$IDZ_BUILD_ROOT/libogg/1.3.1/install-iPhoneOS-armv7
-idz_configure armv7 7.0 libvorbis-1.3.3/configure
+idz_configure armv7 $IDZ_SDK_VERSION libvorbis-$IDZ_VORBIS_VERSION/configure
 export IDZ_EXTRA_CONFIGURE_FLAGS=--with-ogg=$IDZ_BUILD_ROOT/libogg/1.3.1/install-iPhoneOS-armv7s
-idz_configure armv7s 7.0 libvorbis-1.3.3/configure
+idz_configure armv7s $IDZ_SDK_VERSION libvorbis-$IDZ_VORBIS_VERSION/configure
 export IDZ_EXTRA_CONFIGURE_FLAGS=--with-ogg=$IDZ_BUILD_ROOT/libogg/1.3.1/install-iPhoneOS-arm64
-idz_configure arm64 7.0 libvorbis-1.3.3/configure
+idz_configure arm64 $IDZ_SDK_VERSION libvorbis-$IDZ_VORBIS_VERSION/configure
 
 # Simulator build
 export IDZ_EXTRA_CONFIGURE_FLAGS=--with-ogg=$IDZ_BUILD_ROOT/libogg/1.3.1/install-iPhoneSimulator-i386
-idz_configure i386 7.0 libvorbis-1.3.3/configure
+idz_configure i386 $IDZ_SDK_VERSION libvorbis-$IDZ_VORBIS_VERSION/configure
 export IDZ_EXTRA_CONFIGURE_FLAGS=--with-ogg=$IDZ_BUILD_ROOT/libogg/1.3.1/install-iPhoneSimulator-x86_64
-idz_configure x86_64 7.0 libvorbis-1.3.3/configure
+idz_configure x86_64 $IDZ_SDK_VERSION libvorbis-$IDZ_VORBIS_VERSION/configure
 
 # Combine libraries
 pushd install-iPhoneOS-armv7/lib
